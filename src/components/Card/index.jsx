7c259f7cbf3c2347d5e620fcb4d65ca9a6bb9a43
bazene <bazenesergeamos0@gmail.com>
2024-08-26 14:17:38 +0100

    Add styled-components

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,28 +1,32 @@
 import PropTypes from 'prop-types'
 import DefaultPicture from '../../assets/profile.png'
+import styled from 'styled-components'
 
-function Card({ label, title, picture }) {
+const CardLabel = styled.span`
+    color: #5843e4;
+    font-size: 22px;
+    font-weight: bold;
+`
+const CardImage = styled.img`
+    height: 80px;
+    width: 80px;
+    border-radius: 50%;
+`
+
+function Card({ label = "", title = "", picture = {DefaultPicture} }) {
     return (
         <div style={{ display: 'flex', flexDirection: 'column', padding: 15 }}>
-            <span>{label}</span>
-            <img src={picture} alt="freelance" height={80} width={80} />
+            <CardLabel>{label}</CardLabel>
+            <CardImage src={picture} alt="freelance" />
             <span>{title}</span>
         </div>
     )
 }
 
-// The use of propType
 Card.propTypes = {
     label : PropTypes.string.isRequired,
     title : PropTypes.string.isRequired,
     picture : PropTypes.string.isRequired,
 }
-
-// Default props
-Card.defaultProps = {
-    label: '',
-    title: '',
-    picture: DefaultPicture,
-  }
  
 export default Card
