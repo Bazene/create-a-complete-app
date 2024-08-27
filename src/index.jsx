e3c270fde2cda6c90b0d2179b839243eb866d5d3
bazene <bazenesergeamos0@gmail.com>
2024-08-27 22:56:53 +0100

    Use of context to make a darkmode theme

--- src/index.jsx
+++ src/index.jsx
@@ -9,32 +9,33 @@ import FreelanceForm from './components/FreelanceForm'
 import Error from './components/Error'
 import Results from './pages/Results'
 import Freelances from './pages/Freelances'
-import { createGlobalStyle } from "styled-components";
+import { GlobalStyle } from "./utils/style/GlobalStyle";
+import { ThemeProvider } from "./utils/context";
+import Footer from "./components/Footer";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-const GlobalStyle = createGlobalStyle`
-    div {
-        font-family: 'Trebuchet MS', Helvetica, sans-serif;
-    }
-`
 
 root.render(
   <React.StrictMode>
       <Router>
-        <GlobalStyle />
-        <Header />
-        
-        <Routes>
-          <Route path="*" element={<Error />} />
-          <Route path="/" element={<Home />} />
-          <Route path="/results" element={<Results />} />
-          <Route path="/freelances" element={<Freelances />} />
+        <ThemeProvider>
+          <GlobalStyle />
+          <Header />
+          
+          <Routes>
+            <Route path="*" element={<Error />} />
+            <Route path="/" element={<Home />} />
+            <Route path="/results" element={<Results />} />
+            <Route path="/freelances" element={<Freelances />} />
 
-          <Route path="/survey/:questionNumber" element={<Survey />}>
-            <Route path="client" element={<ClientForm />} />
-            <Route path="freelance" element={<FreelanceForm />} />
-          </Route>
-        </Routes>
+            <Route path="/survey/:questionNumber" element={<Survey />}>
+              <Route path="client" element={<ClientForm />} />
+              <Route path="freelance" element={<FreelanceForm />} />
+            </Route>
+          </Routes>
+
+          <Footer />
+        </ThemeProvider>
       </Router>
   </React.StrictMode>
 )
